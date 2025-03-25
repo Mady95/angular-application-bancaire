742bf7345bbe4e2faaef6a278fc6108d54a32bf4
Mady95 <ms.sambake@gmail.com>
2025-03-25 16:07:56 +0100

    application avec les feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { AccountDetailsComponent } from './features/account-details/account-details.component';
 import {CreateAccountComponent} from './create-account/create-account.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import {ProfileComponent} from './profile/profile.component';
 
 export const routes: Routes = [
@@ -10,6 +12,8 @@ export const routes: Routes = [
   { path: 'account/:id', component: AccountDetailsComponent },
   { path: 'create-account', component: CreateAccountComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
 ];
 
@@ -18,3 +22,4 @@ export const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
