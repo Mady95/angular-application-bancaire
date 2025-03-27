648fe8dab697298f529b6b10ce8fa4d6de4bb87e
Mady95 <ms.sambake@gmail.com>
2025-03-27 11:44:20 +0100

    Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone : true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  constructor(private router: Router){}
+
+  logout() {
+    // Logique de déconnexion (peut inclure la suppression du token, etc.)
+    console.log('Déconnexion réussie');
+
+    // Rediriger vers la page de login
+    this.router.navigate(['/login']);
+  }
 }
+
